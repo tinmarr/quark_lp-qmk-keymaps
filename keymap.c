4af97d945775ec717f1e2ada72eb00a34da0795c
Martin Chaperot <martin.chapino@gmail.com>
2024-08-29 22:38:00 -0700

    Go back to SCGA... the otherone was too hard

--- keymap.c
+++ keymap.c
@@ -6,16 +6,16 @@
 
 // Define homerow mods
 // Left-hand home row mods
-#define HOME_A LGUI_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LSFT_T(KC_D)
-#define HOME_F LCTL_T(KC_F)
+#define HOME_A LSFT_T(KC_A)
+#define HOME_S LCTL_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LALT_T(KC_F)
 
 // Right-hand home row mods
-#define HOME_J LCTL_T(KC_J)
-#define HOME_K RSFT_T(KC_K)
-#define HOME_L RALT_T(KC_L)
-#define HOME_SCLN RGUI_T(KC_SCLN)
+#define HOME_J LALT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L RCTL_T(KC_L)
+#define HOME_SCLN RSFT_T(KC_SCLN)
 
 // Open up the array
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
