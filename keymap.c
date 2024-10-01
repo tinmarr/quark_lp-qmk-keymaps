26ef4a81b8db1228f33c731c635f909f7789aef7
Martin Chaperot <martin.chapino@gmail.com>
2024-10-01 16:55:28 -0400

    Add a cool l3 to enable easier gaming

--- keymap.c
+++ keymap.c
@@ -38,7 +38,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,   KC_F12,
     XXXXXXX,  KC_MPRV,  KC_MPLY,  KC_MNXT,  XXXXXXX,  XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  XXXXXXX,  XXXXXXX,
     XXXXXXX,  KC_MUTE,  KC_VOLD,  KC_VOLU,  XXXXXXX,  KC_BRID,  KC_BRIU,  KC_PGDN,  KC_PGUP,  XXXXXXX,   XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_DEL,   _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  KC_PSCR
-)
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  TG(3),    KC_DEL,   _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  KC_PSCR
+),
+
+[3] = LAYOUT_ortho_4x12(
+    KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,   KC_T,     KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,       KC_BSLS,
+    KC_ESC,   KC_A,     KC_S,     KC_D,    KC_F,   KC_G,     KC_H,    KC_J,   KC_K,     KC_L,     KC_SCLN,    KC_QUOT,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,   KC_B,     KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,    KC_ENT,
+    KC_LCTL,  KC_LGUI,  KC_LALT,  KC_APP,  MO(1),  KC_BSPC,  KC_SPC,  TG(3),  KC_LEFT,  KC_DOWN,  KC_UP,      KC_RIGHT
+),
 
 };
