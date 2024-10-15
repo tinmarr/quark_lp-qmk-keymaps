2f770a49385ded64fdbf1000256dc7dbaf573d97
Martin Chaperot <martin.chapino@gmail.com>
2024-10-15 17:03:36 -0400

    Attempt a gaming layer

--- keymap.c
+++ keymap.c
@@ -42,10 +42,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_GAME] = LAYOUT_ortho_4x12(
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,    KC_U,       KC_I,     KC_O,     KC_P,     KC_BSLS,
-    KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,    KC_H,    KC_J,       KC_K,     KC_L,     KC_SCLN,  KC_ENT,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,    KC_M,       KC_COMM,  KC_DOT,   KC_SLSH,  KC_QUOT,
-    KC_LCTL,  KC_LGUI,  KC_LALT,  KC_BSPC,  _______,  KC_SPC,  KC_SPC,  DF(_BASE),  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT
+    KC_LCTL,  KC_Q,     KC_W,    KC_E,     KC_R,    XXXXXXX,  XXXXXXX,  KC_Y,     KC_U,    KC_I,       KC_O,     KC_P,
+    KC_ESC,   KC_A,     KC_S,    KC_D,     KC_F,    XXXXXXX,  XXXXXXX,  KC_H,     KC_J,    KC_K,       KC_L,     KC_SCLN,
+    KC_LSFT,  KC_Z,     KC_X,    KC_C,     KC_V,    XXXXXXX,  XXXXXXX,  KC_N,     KC_M,    KC_COMM,    KC_DOT,   KC_SLSH,
+    XXXXXXX,  XXXXXXX,  KC_TAB,  _______,  KC_SPC,  XXXXXXX,  XXXXXXX,  _______,  KC_ENT,  DF(_BASE),  XXXXXXX,  XXXXXXX
 ),
 
 [_NUM] = LAYOUT_ortho_4x12(
