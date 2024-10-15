5c742bdc5509f9acfd40b695bec0264a7dfa7fd5
Martin Chaperot <martin.chapino@gmail.com>
2024-10-14 21:55:19 -0400

    Attempt at a split 36

--- keymap.c
+++ keymap.c
@@ -28,17 +28,17 @@ enum _Layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT_ortho_4x12(
-    XXXXXXX,  KC_Q,     KC_W,     KC_E,    KC_R,              KC_T,     KC_Y,    KC_U,              KC_I,     KC_O,     KC_P,       XXXXXXX,
-    XXXXXXX,  HOME_A,   HOME_S,   HOME_D,  HOME_F,            KC_G,     KC_H,    HOME_J,            HOME_K,   HOME_L,   HOME_SCLN,  XXXXXXX,
-    XXXXXXX,  KC_Z,     KC_X,     KC_C,    KC_V,              KC_B,     KC_N,    KC_M,              KC_COMM,  KC_DOT,   KC_SLSH,    XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_TAB,  LT(_NUM, KC_ESC),  KC_BSPC,  KC_SPC,  LT(_SYS, KC_ENT),  KC_ALGR,  XXXXXXX,  XXXXXXX,    XXXXXXX
+    KC_Q,     KC_W,     KC_E,    KC_R,              KC_T,     XXXXXXX,  XXXXXXX,  KC_Y,    KC_U,              KC_I,     KC_O,     KC_P,
+    HOME_A,   HOME_S,   HOME_D,  HOME_F,            KC_G,     XXXXXXX,  XXXXXXX,  KC_H,    HOME_J,            HOME_K,   HOME_L,   HOME_SCLN,
+    KC_Z,     KC_X,     KC_C,    KC_V,              KC_B,     XXXXXXX,  XXXXXXX,  KC_N,    KC_M,              KC_COMM,  KC_DOT,   KC_SLSH,
+    XXXXXXX,  XXXXXXX,  KC_TAB,  LT(_NUM, KC_ESC),  KC_BSPC,  XXXXXXX,  XXXXXXX,  KC_SPC,  LT(_SYS, KC_ENT),  KC_ALGR,  XXXXXXX,  XXXXXXX
 ),
 
 [_SYS] = LAYOUT_ortho_4x12(
-    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,   KC_F12,
-    XXXXXXX,  KC_MPRV,  KC_MPLY,  KC_MNXT,  XXXXXXX,    XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  KC_PSCR,  XXXXXXX,
-    XXXXXXX,  KC_MUTE,  KC_VOLD,  KC_VOLU,  XXXXXXX,    KC_BRID,  KC_BRIU,  KC_PGDN,  KC_PGUP,  XXXXXXX,   XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  DF(_GAME),  KC_DEL,   _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX
+    KC_F1,    KC_F2,    KC_F3,    KC_F4,      KC_F5,    XXXXXXX,  XXXXXXX,  KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,
+    KC_MPRV,  KC_MPLY,  KC_MNXT,  XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  KC_PSCR,
+    KC_MUTE,  KC_VOLD,  KC_VOLU,  XXXXXXX,    KC_BRID,  XXXXXXX,  XXXXXXX,  KC_BRIU,  KC_PGDN,  KC_PGUP,  KC_F11,    KC_F12,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  DF(_GAME),  KC_DEL,   XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX
 ),
 
 [_GAME] = LAYOUT_ortho_4x12(
@@ -49,10 +49,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_NUM] = LAYOUT_ortho_4x12(
-    XXXXXXX,  KC_GRV,  KC_TILD,  KC_LPRN,  KC_RPRN,  KC_LCBR,  KC_RCBR,  KC_LBRC,  KC_RBRC,  KC_MINS,  KC_UNDS,  XXXXXXX,
-    XXXXXXX,  KC_1,    KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     XXXXXXX,
-    XXXXXXX,  KC_BSLS, KC_QUOT,  KC_AMPR,  KC_PERC,  KC_CIRC,  KC_DLR,   KC_HASH,  KC_EQL,   KC_PLUS,  KC_ASTR,  _______,
-    _______,  _______, _______,  _______,  _______,  _______,  _______,  XXXXXXX,  _______,  _______,  _______,  _______
+    KC_GRV,  KC_TILD,  KC_LPRN,  KC_RPRN,  KC_LCBR,  XXXXXXX,  XXXXXXX,  KC_RCBR,  KC_LBRC,  KC_RBRC,  KC_MINS,  KC_UNDS,
+    KC_1,    KC_2,     KC_3,     KC_4,     KC_5,     XXXXXXX,  XXXXXXX,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
+    KC_BSLS, KC_QUOT,  KC_AMPR,  KC_PERC,  KC_CIRC,  XXXXXXX,  XXXXXXX,  KC_DLR,   KC_HASH,  KC_EQL,   KC_PLUS,  KC_ASTR,
+    XXXXXXX, XXXXXXX,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX
 
 ),
 
